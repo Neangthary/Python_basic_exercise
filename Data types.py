93c3b801c87c0bd44c16d5dc6247d44683b76e73
NeangThary <100552022+Neangthary@users.noreply.github.com>
2023-03-09 23:55:28 +0700

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,46 @@
+x = 2
+y = 0.58
+z = -1
+a = 7j
+b = -0.9
+c = -5.6j
+d = 3j - 7
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#Type Conversion (Convert from one type to another)
+x = 7   # int
+y = -4.6  # float
+z = 3j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Random Number
+# import random
+
+# print(random.randrange(1, 10))
+import random
+print(random.randrange(10,20))
+
+
